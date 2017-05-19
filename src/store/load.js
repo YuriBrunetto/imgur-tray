9ec9467f9d94a4b95b7f73135cdc75bc398245a8
Yuri Brunetto <brunettoyuri@gmail.com>
2017-05-19 02:24:39 -0300

    copy link to clipboard

--- src/store/load.js
+++ src/store/load.js
@@ -1,5 +1,6 @@
 const KEY = require('./key')
 const openLinks = require('./../openLinks')
+const Clipboard = require('clipboard')
 
 const my_uploads = document.querySelector('#my-uploads')
 
@@ -15,7 +16,8 @@ const load = () => {
           <div class="item-img" style="background-image:url(${img.link})"></div>
           <div class="item-description">
             <a href="${img.link}" title="${img.link}" class="item-a item-uploaded">${img.link}</a>
-            <button type="button" class="item-clipboard">Copy to clipboard</button>
+            <button type="button" class="item-clipboard" data-clipboard-text="${img.link}">Copy to clipboard</button>
+            <span class="item-copied">Copied!</span>
           </div>
         </div>
       `
@@ -24,6 +26,15 @@ const load = () => {
 
       let items = document.querySelectorAll('.item-uploaded')
       openLinks(items)
+    })
+
+    let clipboard = new Clipboard('.item-clipboard')
+    clipboard.on('success', (e) => {
+      let copied = e.trigger.nextElementSibling
+      copied.style.opacity = 1
+      copied.addEventListener('transitionend', () => {
+        copied.style.opacity = 0
+      })
     })
   } else {
     my_uploads.innerHTML = `<p>You haven't uploaded any files yet.</p>`
