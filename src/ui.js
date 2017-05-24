35d5094322988ea72215249e295206fefa10ffcf
Yuri Brunetto <brunettoyuri@gmail.com>
2017-05-24 02:40:31 -0300

    refactoring code

--- src/ui.js
+++ src/ui.js
@@ -2,8 +2,8 @@
 const showTab = require('./showTab')
 const tabs_item = document.querySelectorAll('.tabs-item')
 
-tabs_item.forEach((tab) => {
-  tab.addEventListener('click', (e) => showTab(e.target.id))
+tabs_item.forEach(tab => {
+  tab.addEventListener('click', e => showTab(e.target.id))
 })
 
 showTab('new')
